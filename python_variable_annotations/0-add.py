216e38153e543d31d89e111b314207cff4f2fa15
Josuejosue1999 <j.biaou@alustudent.com>
2024-09-04 22:35:56 +0200

    Done

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,11 +3,9 @@
 def add(a: float, b: float) -> float:
     """
     This function takes two floating-point numbers, a and b, and returns their sum.
-
     Args:
         a (float): The first floating-point number.
         b (float): The second floating-point number.
-
     Returns:
         float: The sum of the two floating-point numbers.
     """
