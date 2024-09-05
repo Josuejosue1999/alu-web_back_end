fd26e9ad5520473a3b759ab664ffb92050a8cb02
Josuejosue1999 <j.biaou@alustudent.com>
2024-09-05 13:41:10 +0200

    Done

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+This module contains a function `to_kv` that takes a string and an integer
+or float, and returns a tuple where the first element is the string and
+the second element is the square of the number as a float.
+"""
+
 from typing import Tuple, Union
 
 
@@ -12,6 +18,6 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v (Union[int, float]): The integer or floating-point number.
     Returns:
         Tuple[str, float]: A tuple where the first element is k and the second
-                           element is the square of v as a float.
+        element is the square of v as a float.
     """
     return (k, float(v ** 2))
