c152d6f17eb2865092f4493834f56ca1e9f74ace
Josuejosue1999 <j.biaou@alustudent.com>
2024-09-06 21:58:27 +0200

    Done

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,6 +10,8 @@ from importlib import import_module
 # Import dynamically the 'wait_random' function from '0-basic_async_syntax'
 basic_async_syntax = import_module('0-basic_async_syntax')
 wait_random = basic_async_syntax.wait_random
+
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Creates an asyncio task for the wait_random coroutine.
