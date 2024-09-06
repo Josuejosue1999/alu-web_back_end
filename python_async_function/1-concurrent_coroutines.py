22e4b5c9ad3169a32207f9fc99c726f073e9ffcc
Josuejosue1999 <j.biaou@alustudent.com>
2024-09-06 21:02:29 +0200

    Done

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -18,11 +18,12 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronously spawns `wait_random` n times with
     the specified max_delay
-    and returns a list of all the delays in ascending order without using sort().
+    and returns a list of all the delays in ascending
+    order without using sort().
     
     Args:
     n (int): The number of times to spawn `wait_random`.
-        max_delay (int): The maximum delay for `wait_random`.
+    max_delay (int): The maximum delay for `wait_random`.
     
     Returns:
         List[float]: A list of delays in ascending order.
