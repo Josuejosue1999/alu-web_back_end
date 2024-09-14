51ed3c3fb4d7ae5904d0b8607f7631b6f739769c
Josuejosue1999 <j.biaou@alustudent.com>
2024-09-14 14:27:23 +0200

    Done

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
-""" Returns a salted, hashed password, byte in string """
+"""
+    Encrypt a string
+"""
 import bcrypt
 
 
-def hash_password(password: str) -> bytes:
-    """ Returns byte string password """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+def hash_password(password: str = '') -> bytes:
+    """
+        Hashed the password
+
+        Args:
+            password: string to hashed
+
+        Return:
+            hashed password
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'),
+                           bcrypt.gensalt(prefix=b"2b"))
+
+    return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Implement is_valid to validate provided password
-    matched hashed_password
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+        Look if is valid password
+
+        Args:
+            hashed_password: Password encrypt
+            password: string to hashed
+
+        Return:
+            True If this are equals
+    """
+    valid = bcrypt.checkpw(password.encode('utf-8'),
+                           hashed_password)
+
+    return valid
